4789a7e380d70f4056593232584fe395c2f51a4d
Sarthak Sharma <ssarthak116@protonmail.ch>
2025-02-05 11:42:51 +0530

    grpc servers theoretical implementation: starting integration now

--- utils/nodes/main.go
+++ utils/nodes/main.go
@@ -1,37 +1,83 @@
 package nodes
 
 import (
+	context "context"
+	"fmt"
 	"log"
+	"net"
+	"os"
+	"os/signal"
 
 	"github.com/SuperALKALINEdroiD/timelyDB/config"
+	"google.golang.org/grpc"
 )
+
+type internalServer struct {
+	UnimplementedNodeServiceServer
+}
 
 type Node struct {
 	ID string
 }
 
-func NodeSeupTask(nodeID string) (*Node, error) {
+func NodeSetupTask(ctx context.Context, nodeID string) (*Node, error) {
+	listener, httpError := net.Listen("tcp", ":5000") // TODO
+	if httpError != nil {
+		return nil, fmt.Errorf("failed to start listener: %v", httpError)
+	}
 
-	return &Node{
-		ID: nodeID,
-	}, nil
+	grpcServer := grpc.NewServer()
+	dataStoreServer := &internalServer{}
+	RegisterNodeServiceServer(grpcServer, dataStoreServer)
+
+	stop := make(chan struct{})
+
+	go func() {
+		log.Printf("Node %s: gRPC server started", nodeID)
+		if err := grpcServer.Serve(listener); err != nil {
+			log.Printf("gRPC server error: %v", err)
+		}
+		close(stop)
+	}()
+
+	go func() {
+		<-ctx.Done() // context cancelled
+		log.Printf("Shutting down gRPC server for Node %s...", nodeID)
+		grpcServer.GracefulStop()
+		listener.Close()
+		close(stop)
+	}()
+
+	return &Node{ID: nodeID}, nil
 }
 
 func LoadNodes(config *config.DatabaseConfig) []*Node {
 	if len(config.Nodes) == 0 || config.NodeCount == 0 {
-		log.Fatalln("No node configuration found.")
-		return nil
+		log.Println("No node configuration found.")
+		return []*Node{}
 	}
 
 	log.Println("Loading nodes...")
 
-	var nodes []*Node = make([]*Node, len(config.Nodes))
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	signalChannel := make(chan os.Signal, 1)
+	signal.Notify(signalChannel) // capture all signals on channel
+
+	go func() {
+		<-signalChannel
+		log.Println("Received shutdown signal. Stopping servers...")
+		cancel() // context cancelled: shutdown the servers
+	}()
+
+	grpcNodes := make([]*Node, len(config.Nodes))
 
 	for i, node := range config.Nodes {
 		log.Printf("Node %d: Endpoint ==> %s\n", i+1, node.Endpoint)
 
 		var setupError error
-		nodes[i], setupError = NodeSeupTask(string(rune(i)))
+		grpcNodes[i], setupError = NodeSetupTask(ctx, fmt.Sprintf("%d", i))
 		if setupError != nil {
 			log.Printf("Error setting up Node %d: %v\n", i+1, setupError)
 			continue
@@ -39,5 +85,8 @@ func LoadNodes(config *config.DatabaseConfig) []*Node {
 	}
 
 	log.Println("Nodes are up and running.")
-	return nodes
+	<-ctx.Done()
+
+	log.Println("All gRPC servers have been shut down.")
+	return grpcNodes
 }
